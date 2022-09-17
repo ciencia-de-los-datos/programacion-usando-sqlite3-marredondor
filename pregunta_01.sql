e99107de822076388c3e885ba66a58602579b3e2
marredondor <marredondor@unal.edu.co>
2022-09-17 09:09:40 -0500

    Update pregunta_01.sql

--- pregunta_01.sql
+++ pregunta_01.sql
@@ -17,15 +17,22 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
-CREATE TABLE tbl1 (
-    K0  CHAR(1),
-    K1  INT,
-    c12 FLOAT,
-    c13 INT,
-    c14 DATE,
-    c15 FLOAT,
-    c16 CHAR(4)
-    );
-    
-INSERT INTO tbl1 VALUES (?,?,?,?,?,?,?)", tbl1
-SELECT sum (c12) FROM tbl1
+import pandas as pd
+import sqlite3
+tbl1 = pd.read_csv('tbl1.csv', 
+                      sep=",", 
+                      header = None, 
+                      decimal=".",
+                      names = ['K0','K1','c12','c13','c14','c15','c16'])
+
+conn = sqlite3.connect(":memory:")  ## aca se indica el nombre de la db.
+cur = conn.cursor()
+
+tbl1.to_sql(
+    name="tbl1",
+    con=conn,
+    if_exists="replace")
+
+respuesta = cur.execute("SELECT sum (c12) FROM tbl1").fetchall()
+respuesta = { "0": respuesta[0][0]}
+respuesta
