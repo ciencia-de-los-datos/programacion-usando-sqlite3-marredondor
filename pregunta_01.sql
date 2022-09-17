a5a74d2d68d3a1b91c86bdf18b24ad72d82e2325
marredondor <marredondor@unal.edu.co>
2022-09-17 08:54:14 -0500

    Update pregunta_01.sql

--- pregunta_01.sql
+++ pregunta_01.sql
@@ -18,4 +18,35 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 --
 
-SELECT sum (c12) FROM tbl1
+def open_file (name_file):
+    with open(name_file, "r") as file:
+        data = file.readlines()
+    
+    data = [row.replace("\n", "") for row in data]
+    data = [row.replace("\t", ",") for row in data]
+    data = [row.split(",") for row in data]
+    data = [tuple(line) for line in data]
+    return data
+
+tbl1 = open_file('tbl1.csv') 
+
+conn = sqlite3.connect(":memory:")  ## aca se indica el nombre de la db.
+cur = conn.cursor()
+
+conn.executescript(
+"""
+DROP TABLE IF EXISTS truck_events;
+
+CREATE TABLE tbl1 (K0  CHAR(1),
+                   K1  INT,
+                   c12 FLOAT,
+                   c13 INT,
+                   c14 DATE,
+                   c15 FLOAT,
+                   c16 CHAR(4));
+"""
+)
+conn.commit()
+	
+cur.executemany("INSERT INTO tbl1 VALUES (?,?,?,?,?,?,?)", tbl1)
+cur.execute("SELECT sum (c12) FROM tbl1").fetchall()
