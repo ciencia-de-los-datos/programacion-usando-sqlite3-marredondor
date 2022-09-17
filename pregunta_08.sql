509c46daaea5ae3b847a28e2f28538ad8bc095b1
marredondor <marredondor@unal.edu.co>
2022-09-17 18:01:13 -0500

    Update pregunta_08.sql

--- pregunta_08.sql
+++ pregunta_08.sql
@@ -42,7 +42,7 @@
 --  >>> Escriba su codigo a partir de este punto <<<
 --
 SELECT 
-    STRFTIME('%Y',[C23]),
-    avg(c21) 
+    STRFTIME('%Y',C23),
+    AVG(c21) 
 FROM tbl2
-GROUP BY STRFTIME('%Y',[C23])
+GROUP BY STRFTIME('%Y',C23)
